1d6dd8ef7b78d941b235b01b83729ee3f9b4da95
David A. Viramontes <dviramontes@gmail.com>
2018-03-04 12:59:08 -0700

    feat: adds Desk.rm_ss fn

--- lib/desk.ex
+++ lib/desk.ex
@@ -1,18 +1,31 @@
 defmodule Desk do
+
   @moduledoc """
-  Documentation for Desk.
+  Various utils for ~/Desktop management
   """
 
+  @home "/Users/mmm/Desktos"
+
   @doc """
-  Hello world.
+  Removes stale Screenshots
 
   ## Examples
 
-      iex> Desk.hello
-      :world
+      iex> Desk.rm_ss
+      [:ok]
 
   """
-  def hello do
-    :world
+  def rm_ss() do
+    case File.ls(@home) do
+      {:ok, files} -> Enum.filter(files, fn(file) ->
+        not(File.dir?(@home <> file))
+      end) |> Enum.filter(fn(s) ->
+        case s do
+          "Screen Shot " <> _ -> true
+          _ -> false
+        end
+      end) |> Enum.map(fn(ss) -> File.rm(@home <> ss) end)
+      {:error, error} -> IO.puts "#{error}: Screenshot directory not found"
+    end
   end
 end
